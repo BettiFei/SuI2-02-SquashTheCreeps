11d7c108bd0238c810bd83239e1afd936a99a90d
BettiFei <bettina.feibel@uni-bayreuth.de>
2025-11-15 10:57:48 +0100

    Jump & squash mechanics

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody3D
 
 @export var speed = 14
 @export var fall_acceleration = 75
+@export var jump_impulse = 20
+@export var bounce_impulse = 16
 
 var target_velocity = Vector3.ZERO
 
@@ -30,6 +32,30 @@ func _physics_process(delta: float) -> void:
 	# vertical velocity:
 	if not is_on_floor():
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+	
+	# jumping:
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
+	
+	# SQUASHING & BOUNCING
+	# iterating over all collisions:
+	for index in range(get_slide_collision_count()):
+		var collision = get_slide_collision(index)
+		
+		# when iterating with the same object twice, second collision will return null -> make sure to continue anyway:
+		if collision.get_collider() == null:
+			continue
+		
+		# if colliding with a mob:
+		if collision.get_collider().is_in_group("mob"):
+			var mob = collision.get_collider()
+			# if player hits mob from above, squash it & bounce off:
+			#print(Vector3.UP.dot(collision.get_normal()))
+			if Vector3.UP.dot(collision.get_normal()) > 0.6:
+				mob.squash()
+				target_velocity.y = bounce_impulse
+				# prevent further duplicate calls:
+				break
 	
 	# moving the character:
 	velocity = target_velocity
